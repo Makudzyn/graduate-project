5af6f5e9f033c77e89969df7cad94da25fc1155c
Maksim Kunchenko <72383565+Makudzyn@users.noreply.github.com>
2023-10-07 17:25:19 +0300

    Created responsive UI for Login/Auth pages, created a templated of the responsive Navbar with burger menu

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,8 +1,18 @@
 import {useContext} from "react";
 import {Context} from "../main.tsx";
-import {ADMIN_PANEL_ROUTE, LOGIN_ROUTE, POLYNOMIALS_ROUTE} from "../utils/consts.ts";
-import {useNavigate} from "react-router-dom";
+import {
+    ADMIN_PANEL_ROUTE,
+    LINEAR_GEN_ROUTE,
+    LOGIN_ROUTE,
+    MAIN_ROUTE,
+    MATRIX_GEN_ROUTE,
+    POLYNOMIALS_ROUTE, SUM_AND_MULTI_GEN_ROUTE, UNTITLED_ROUTE
+} from "../utils/consts.ts";
+import {NavLink, useNavigate} from "react-router-dom";
 import {observer} from "mobx-react-lite";
+import userIco from "../assets/user-login.svg";
+import bigDataIco from "../assets/big-data.svg";
+import burgerMenuIco from "../assets/burger-menu.svg";
 
 const NavBar = observer(() => {
     const {userStore} = useContext(Context)!;
@@ -14,40 +24,98 @@ const NavBar = observer(() => {
     }
 
     return (
-        <div className={"bg-black"}>
-           <div>
-               <nav className={"text-white"}>Graduate Project</nav>
-               {userStore.isAuth ?
-                   <nav className="ml-auto">
-                       <div>
-                           <svg width={38} height={38} viewBox="0 0 38 38">
-                               <image
-                                   xlinkHref={'#'}
-                                   width="38"
-                                   height="38"
-                                   onClick={() => navigate(POLYNOMIALS_ROUTE)}
-                                   cursor={"pointer"}
-                               />
+        <header className="antialiased">
+           <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+               <div className="flex flex-wrap justify-between items-center">
+                   <div className="flex justify-start items-center">
+                       <NavLink
+                           className="flex mr-4"
+                           to={MAIN_ROUTE}
+                       >
+                           <img src={bigDataIco} className="mr-3 h-6 sm:h-9" alt="Logo" />
+                           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Graduate Project</span>
+                       </NavLink>
+                   </div>
+                   <div className="flex items-center lg:order-2">
+                       {userStore.isAuth ?
+                           <nav className="ml-auto">
+                               <div>
+                                   <svg width={38} height={38} viewBox="0 0 38 38">
+                                       <image
+                                           xlinkHref={'#'}
+                                           width="38"
+                                           height="38"
+                                           onClick={() => navigate(POLYNOMIALS_ROUTE)}
+                                           cursor={"pointer"}
+                                       />
+                                   </svg>
+                                   <div
+                                       onClick={() => navigate(ADMIN_PANEL_ROUTE)}
+                                   >
+                                       Admin panel
+                                   </div>
+                                   <div
+                                       onClick={() => logOut()}
+                                   >
+                                       Log out
+                                   </div>
+                               </div>
+                           </nav>
+                           :
+                           <nav className="ml-auto">
+                               <svg width={38} height={38} viewBox="0 0 38 38">
+                                   <image
+                                       xlinkHref={userIco}
+                                       width="38"
+                                       height="38"
+                                       onClick={() => navigate(LOGIN_ROUTE)}
+                                       cursor={"pointer"}
+                                   />
+                               </svg>
+                           </nav>
+                       }
+                       <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                           <span className="sr-only">Open main menu</span>
+                           <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                               <image xlinkHref={burgerMenuIco} cursor="pointer" className="w-5 h-5"/>
                            </svg>
-                           <div
-                               onClick={() => navigate(ADMIN_PANEL_ROUTE)}
-                           >
-                               Admin panel
-                           </div>
-                           <div
-                               onClick={() => logOut()}
-                           >
-                               Log out
-                           </div>
-                       </div>
-                   </nav>
-                   :
-                   <nav className="ml-auto">
-                       <div onClick={() => navigate(LOGIN_ROUTE)}>Sign in</div>
-                   </nav>
-               }
-           </div>
-        </div>
+                       </button>
+                   </div>
+                   <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+                           <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0"> {/*li*5>NavLink[to={}]{Page $}*/}
+                               <li>
+                                   <NavLink
+                                       className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page"
+                                       to={MAIN_ROUTE}
+                                   >
+                                       Main
+                                   </NavLink>
+                               </li>
+                               <li>
+                                   <NavLink
+                                       className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                                       to={LINEAR_GEN_ROUTE}>Page 1</NavLink>
+                               </li>
+                               <li>
+                                   <NavLink
+                                       className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                                       to={MATRIX_GEN_ROUTE}>Page 2</NavLink>
+                               </li>
+                               <li>
+                                   <NavLink
+                                       className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                                       to={SUM_AND_MULTI_GEN_ROUTE}>Page 3</NavLink>
+                               </li>
+                               <li>
+                                   <NavLink
+                                       className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                                       to={UNTITLED_ROUTE}>Page 4</NavLink>
+                               </li>
+                           </ul>
+                   </div>
+               </div>
+           </nav>
+        </header>
     );
 });
 
--- client/src/pages/AuthorizationPage.tsx
+++ client/src/pages/AuthorizationPage.tsx
@@ -4,6 +4,8 @@ import {useContext, useState} from "react";
 import {Context} from "../main.tsx";
 import {login, registration} from "../http/userAPI.ts";
 import {observer} from "mobx-react-lite";
+import bigDataIco from "../assets/big-data.svg";
+
 
 const AuthorizationPage = observer(() => {
     const {userStore} = useContext(Context)!; // Данные о пользователе из стора
@@ -30,36 +32,108 @@ const AuthorizationPage = observer(() => {
     }
 
     return (
-        <div>
-            <div>
-                <h2>{isLogin ? "Authorization" : "Registration"}</h2>
-                <div>
-                    <input
-                        placeholder={"Enter your e-mail here..."}
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                    <input
-                        placeholder={"Enter your password here..."}
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        type={"password"}
-                    />
-                    <button onClick={authOrLogin}>
-                        {isLogin ? "Sign in" : "Sign up"}
-                    </button>
+        <div className={"bg-gray-50 dark:bg-gray-900 font-sans"}>
+            <div className={"flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"}>
+                <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                    <img className="w-8 h-8 mr-2" src={bigDataIco} alt="logo"/>
+                        {isLogin ? "Authorization" : "Registration"}
+                </a>
+                <div className={"w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"}>
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Sign in to your account
+                        </h1>
+                        <form
+                            className="space-y-4 md:space-y-6"
+                            action="#"
+                        >
+                            <div>
+                                <label
+                                    htmlFor="email"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Your email
+                                </label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 "
+                                    placeholder="name@company.com"
+                                    required={true}
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor="password"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    placeholder="••••••••"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 placeholder:text-xl"
+                                    required={true}
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                />
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <div className="flex items-start">
+                                    <div className="flex items-center h-5">
+                                        <input
+                                            id="remember"
+                                            aria-describedby="remember"
+                                            type="checkbox"
+                                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                                        />
+                                    </div>
+                                    <div className="ml-3 text-sm">
+                                        <label
+                                            htmlFor="remember"
+                                            className="text-gray-500 dark:text-gray-300">
+                                            Remember me
+                                        </label>
+                                    </div>
+                                </div>
+                                <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">
+                                    Forgot password?
+                                </a>
+                            </div>
+                            <button
+                                onClick={authOrLogin}
+                                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            >
+                                {isLogin ? "Sign in" : "Sign up"}
+                            </button>
 
-                    {isLogin ?
-                        <div>
-                            Don't have an account? &nbsp;
-                            <NavLink to={REGISTRATION_ROUTE}/>
-                        </div>
-                        :
-                        <div>
-                            Sign up
-                            <NavLink to={LOGIN_ROUTE}/>
-                        </div>
-                    }
+                            {isLogin ?
+                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                    Don’t have an account yet?
+                                    <NavLink
+                                        to={REGISTRATION_ROUTE}
+                                        className="font-medium text-primary-600 hover:underline dark:text-primary-500 ml-1"
+                                    >
+                                        Sign up here
+                                    </NavLink>
+                                </p>
+                                :
+                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                    Already have an account?
+                                    <NavLink
+                                        to={LOGIN_ROUTE}
+                                        className="font-medium text-primary-600 hover:underline dark:text-primary-500 ml-1"
+                                    >
+                                        Login here
+                                    </NavLink>
+                                </p>
+                            }
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
