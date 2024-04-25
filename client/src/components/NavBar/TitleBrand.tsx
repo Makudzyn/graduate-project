8cd70a41965be4d0112050aedffda4611c4b1030
Makudzyn <alt4xh0@gmail.com>
2024-04-25 18:19:21 +0300

    Modal styles, validity function
    Fixed look of modal window, added warning icon. Extracted validity check function from Linear generator page to functions file, changed language of error description

--- client/src/components/NavBar/TitleBrand.tsx
+++ client/src/components/NavBar/TitleBrand.tsx
@@ -4,7 +4,7 @@ import BigDataIcon from "../../assets/big-data.svg?react";
 
 const TitleBrand = () => {
   return (
-    <div className="flex flex-shrink-0 items-center max-md:ml-3">
+    <div className="flex flex-shrink-0 items-center md:ml-3">
       <NavLink className="flex md:mr-0 lg:mr-4" to={MAIN_ROUTE}>
         <BigDataIcon className="mr-3 md:mr-0 lg:mr-3.5 w-9 h-9" />
         <span className="self-center sm:text-2xl md:hidden  lg:text-xl lg:block font-semibold whitespace-nowrap text-white">
--- client/src/pages/LinearGeneratorPage.tsx
+++ client/src/pages/LinearGeneratorPage.tsx
@@ -15,8 +15,7 @@ import Spinner from "../components/Spinner.tsx";
 import SideBar from "../components/SideBar/SideBar.tsx";
 import useHistoryFetching from "../hooks/fetching/useHistoryFetching.ts";
 import { handleHistoryRecordCreation } from "../functions/requestFunctions/requestFunctions.ts";
-import { getSelectedParam } from "../functions/functions.ts";
-import { polynomialDestructuring } from "../functions/generatorFunctions.ts";
+import { validityCheck } from "../functions/functions.ts";
 import Modal from "../components/Modal/Modal.tsx";
 
 const LinearGeneratorPage = observer(() => {
@@ -44,39 +43,8 @@ const LinearGeneratorPage = observer(() => {
 
   const [searchParams, setSearchParams] = useSearchParams("");
 
-  const validityCheck = () => {
-    const degree = parseInt(getSelectedParam(PARAMS_DEGREE, searchParams));
-    if (degree === undefined || degree === null || isNaN(degree)) {
-      setError("Invalid degree number.");
-      return false;
-    }
-
-    const polynomial = getSelectedParam(PARAMS_POLYNOMIAL, searchParams);
-    if (polynomial === null) {
-      setError("Invalid polynomial.");
-      return false;
-    }
-    const { polyBinary } = polynomialDestructuring(polynomial);
-
-    if (polyBinary.length - 1 !== degree) {
-      setError("The selected polynomial does not correspond to the selected degree.");
-      return false;
-    }
-    const paramValue = getSelectedParam(PARAMS_USER_VALUE, searchParams);
-    if (paramValue === null) {
-      setError("Invalid user value (initial state).");
-      return false;
-    }
-    console.log(polyBinary.length, paramValue.length);
-    if (polyBinary.length !== paramValue.length + 1) {
-      setError("Length of user value (initial state) does not equal to polynomial length.");
-      return false;
-    }
-    return true;
-  }
-
   const handleClick = () => {
-    const isValid = validityCheck();
+    const isValid = validityCheck(searchParams, PARAMS_DEGREE, PARAMS_POLYNOMIAL, PARAMS_USER_VALUE, setError);
     if (isValid) {
       userStore.isAuth && handleHistoryRecordCreation(userStore.user.id);
       linearCalculations(
