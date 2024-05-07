1e8d5ab84356532a6c130afed4dfddce75bbfa84
Makudzyn <alt4xh0@gmail.com>
2024-05-07 12:30:20 +0300

    Finalized view of Features block, minor style fixes, closed #10
    Extracted card rows into a separate component, added a new row, made changes to the contents of the cards, added icons representing card feature. Style fixes in accordion item.

--- client/src/components/MainPage/AccordionItem.tsx
+++ client/src/components/MainPage/AccordionItem.tsx
@@ -13,14 +13,14 @@ const AccordionItem = ({ title, description }: AccordionItemProps) => {
         <>
           <Disclosure.Button
             className={`${open ? "shadow-white/75 shadow-md" : "hover:shadow-purpleFirst/75 hover:shadow-md"}
-              flex w-full justify-between rounded-lg p-6 text-left text-base transition-shadow font-medium text-gray-50
+              flex w-full w-max-screen-lg justify-between rounded-lg p-6 text-left text-base transition-shadow font-medium text-gray-50
               focus:outline-none focus-visible:ring focus-visible:ring-purpleFirst/75`}
           >
             <h4>{title}</h4>
             <ChevronIcon
               className={`${
                 open ? "rotate-180 transform" : ""
-              } h-5 w-5 stroke-gray-50 transition-transform`}
+              } h-6 w-6 stroke-gray-50 transition-transform`}
             />
           </Disclosure.Button>
           <Transition
@@ -33,7 +33,7 @@ const AccordionItem = ({ title, description }: AccordionItemProps) => {
             leaveTo="transform opacity-0 scale-95"
           >
             <Disclosure.Panel
-              className={`px-6 pb-5 pt-4 text-sm+ text-secondary`}
+              className="px-6 pb-5 pt-4 text-sm+ text-secondary max-w-screen-lg"
               static
             >
               {description}
--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,10 +1,3 @@
-import Card from "../components/MainPage/Card.tsx";
-import TableIcon from "../assets/table-tree.svg?react";
-import ChartIcon from "../assets/chart-line.svg?react";
-import DataIcon from "../assets/data.svg?react";
-import StatsIcon from "../assets/stats.svg?react";
-import FastIcon from "../assets/speed-alt.svg?react";
-import SmileIcon from "../assets/slightly-smiling-face.svg?react";
 import BlockWrapper from "../components/MainPage/BlockWrapper.tsx";
 import Step from "../components/MainPage/Step.tsx";
 import AccordionItem from "../components/MainPage/AccordionItem.tsx";
@@ -12,6 +5,7 @@ import SectionTitle from "../components/MainPage/SectionTitle.tsx";
 import SectionBlock from "../components/MainPage/SectionBlock.tsx";
 import Section from "../components/CommonGenComponents/Section.tsx";
 import PageHeader from "../components/CommonGenComponents/PageHeader.tsx";
+import FeaturesBlock from "../components/MainPage/FeaturesBlock.tsx";
 
 const MainPage = () => {
   return (
@@ -38,90 +32,13 @@ const MainPage = () => {
             "
             alignCenter
           />
-          <div className="flex flex-wrap justify-between">
-            <Card
-              iconComponent={
-                <TableIcon className="mb-4 h-6 w-6 fill-transparent stroke-secondary" />
-              }
-              title="Таблиця поліномів"
-              description="
-                Дослідіть світ можливостей з великою таблицею,
-                що надає повний список поліномів для створення послідовностей.
-                Таблиця містить найбільший набір поліномів, доступний в інтернеті, і пропонує зручні функції пошуку,
-                сортування та інші засоби взаємодії, забезпечуючи легкий доступ до необхідної інформації.
-              "
-            />
-            <Card
-              iconComponent={
-                <ChartIcon className="mb-4 h-6 w-6 fill-transparent stroke-secondary" />
-              }
-              title="Графіки кореляції"
-              description="
-                Досліджуйте кореляцію згенерованих послідовностей за допомогою графіків кореляції.
-                Інтуїтивно зрозумілі графіки дозволяють оцінити ступінь кореляції та вивчити властивості послідовності.
-                Додаткові графічні інтрументи для взаємодії з графіком
-                забезпечують більш точне та глибоке розуміння даних,
-                допомагаючи краще аналізувати створені послідовності.
-              "
-            />
-            <Card
-              iconComponent={
-                <DataIcon className="mb-4 h-6 w-6 fill-transparent stroke-secondary" />
-              }
-              title="Збереження даних"
-              description="
-                Зареєстровані користувачі можуть легко зберігати
-                та керувати історією своїх запитів та даних для генерації послідовностей.
-                Це забезпечує зручність повторного використання раніше введених даних та запитів,
-                дозволяючи економити час та зосереджуватися на дослідженні та аналізі результатів.
-              "
-            />
-          </div>
-          <div className="flex flex-wrap justify-between">
-            <Card
-              iconComponent={
-                <StatsIcon className="mb-4 h-6 w-6 fill-transparent stroke-secondary" />
-              }
-              title="Порівняння характеристик"
-              description="
-                На сайті надається унікальна можливість порівняти характеристики та властивості
-                матричного та лінійного генераторів безпосередньо поруч один з одним.
-                За допомогою спеціальної сторінки можна провести детальний аналіз ваги Хеммінгу для різних блоків згенерованих послідовностей,
-                що допоможе вибрати найбільш підходящий генератор для конкретних потреб та задач.
-              "
-            />
-            <Card
-              iconComponent={
-                <FastIcon className="mb-4 h-6 w-6 fill-transparent stroke-secondary" />
-              }
-              title="Швидкість"
-              description="
-                Завдяки виконанню обчислень на сервері користувачі миттєво отримують результати,
-                забезпечуючи швидкий та плавний досвід користування.
-                Навіть під час роботи з потенційно великими обсягами даних на сайті немає затримок завдяки
-                використанню технології windowing для ефективного відображення результатів.
-              "
-            />
-            <Card
-              iconComponent={
-                <SmileIcon className="mb-4 h-6 w-6 fill-transparent stroke-secondary" />
-              }
-              title="Зручний дизайн"
-              description="
-                Сайт пропонує зручний та привабливий інтерфейс,
-                який навіть новачки можуть легко освоїти.
-                Використовуючи різноманітні повідомлення, плейсхолдери та тултипи,
-                забезпечується проста та інтуїтивно зрозуміла взаємодію з сайтом,
-                що робить процес дослідження та генерації послідовностей максимально зручним для всіх користувачів.
-          "
-            />
-          </div>
+          <FeaturesBlock/>
         </BlockWrapper>
       </SectionBlock>
       <SectionBlock>
         <BlockWrapper>
           <SectionTitle
-            headerTitle="How it works"
+            headerTitle="Як це працює"
             paragraphText="Show step by step how to get started"
             alignCenter
           />
@@ -164,11 +81,10 @@ const MainPage = () => {
       <SectionBlock>
         <BlockWrapper>
           <SectionTitle
-            headerTitle="Common Queries"
+            headerTitle="Питання та відповіді"
             paragraphText="
-              Have questions?
-              We’ve compiled a list of the most frequently asked questions
-              to help you get to know BitSequencePro better.
+              Є питання? У цьому розділі складено список найпоширеніших запитань,
+              щоб допомогти вам краще пізнати цей застосунок.
             "
           />
           <div className="flex bg-bgSecondary text-secondary box-border border-0 shadow-lg rounded-md flex-col ">
